67d043f42280e0c1bb1af06d1c3baa37c7a0072d
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2021-11-30 14:23:54 +0200

    Implemented adding of new cars

--- CarRentingSystem/Controllers/HomeController.cs
+++ CarRentingSystem/Controllers/HomeController.cs
@@ -1,37 +1,17 @@
-﻿using CarRentingSystem.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CarRentingSystem.Controllers
+﻿namespace CarRentingSystem.Controllers
 {
+    using System.Diagnostics;
+    using CarRentingSystem.Models;
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ CarRentingSystem/Data/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRentingSystem.Data.Models
+{
+    using System.Collections.Generic;
+    public class Category
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Car> Cars { get; init; } = new HashSet<Car>();
+    }
+}
